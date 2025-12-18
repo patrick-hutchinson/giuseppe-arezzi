fb9769de0689ec96011dcb467a4d80dfc5e61b5e
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2025-12-18 12:03:21 +0100

    Updated Mobile line break

--- app/src/app/(home)/Home.jsx
+++ app/src/app/(home)/Home.jsx
@@ -7,7 +7,14 @@ export default function HomePage({ site, home }) {
     <main className={styles.main}>
       <div className={styles.announcement_container}>
         <div>{site.title}</div>
-        <Text text={home.announcement} />
+        {/* <Text text={home.announcement} /> */}
+
+        <p className={styles.announcement_text}>
+          New website under construction. <br />
+          You can contact me at&nbsp;
+          <a href="mailto:info@giuseppearezzi.com">info@giuseppearezzi.com</a>
+        </p>
+
         <div>
           {site.socials.map((social, index) => (
             <a key={index} href={social.link} target="_blank">
